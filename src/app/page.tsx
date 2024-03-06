c85ac9bb4b70b5c99b910457395ab52c3c6a6e7f
Dali bendhiab <itsdalibendhiab@gmail.com>
2024-03-06 23:45:06 +0100

    Add redirect to '/70s' page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,13 @@
 import HomeTwo from "@/components/homes/home-two";
 import Wrapper from "@/layout/Wrapper";
+import { redirect } from 'next/navigation'
 
 export const metadata = {
   title: "JAT SOUSSE | Accueil",
 };
 const index = () => {
-  return (
-<>
-</>
-  )
+  return redirect('/70s') 
+
 }
 
 export default index
