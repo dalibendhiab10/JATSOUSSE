e154d0d7d7c07634487e0ce82070272c285a2c60
Dali bendhiab <itsdalibendhiab@gmail.com>
2024-03-11 22:22:39 +0100

    Update ESLint configuration and Next.js config, and remove unused authentication route

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,12 +1,15 @@
-import NextAuth from 'next-auth';
+import NextAuth , { AuthOptions } from 'next-auth';
 import CredentialsProvider from 'next-auth/providers/credentials';
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import { PrismaClient } from '@prisma/client';
 import bcrypt from 'bcryptjs';
 
 const prisma = new PrismaClient();
-
-export const authOptions = {
+interface Credentials {
+ email: string;
+ password: string;
+}
+export const authOptions: AuthOptions = {
  providers: [
     CredentialsProvider({
       name: 'Credentials',
