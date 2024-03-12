bebabf4800158c48a65aef60f60aeb32f792e04d
Dali bendhiab <itsdalibendhiab@gmail.com>
2024-03-12 22:58:15 +0100

    Remove BarcodeScanner component and update page.tsx

--- src/app/(dashboard)/admin/dash/(scanphy)/page.tsx
+++ src/app/(dashboard)/admin/dash/(scanphy)/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 import React, { useEffect, useRef, useState } from "react";
 import { useZxing } from 'react-zxing';
-import BarcodeScannerComponent from "./BarcodeScanner"
 const ScanBarcodePage = () => {
     const [data, setData] = React.useState("Not Found");
       
@@ -9,15 +8,7 @@ const ScanBarcodePage = () => {
     <>
       <div>
         <h1>Scan Barcode</h1>
-        <BarcodeScannerComponent
-            setBarcode={(barcode) => {
-                setData(barcode);
-            }}
-            setError={(error) => {
-                setData(error);
-            }}
         
-        />
       <p>{data}</p>
       </div>
     </>
